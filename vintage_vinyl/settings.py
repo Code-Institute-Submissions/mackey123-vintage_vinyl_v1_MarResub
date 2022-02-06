037a8ec58f7af1d57ec1e507b36ee829e5bc3c44
mackey123 <seanrobert2010@gmail.com>
2022-02-06 15:31:29 +0000

    make migrations again

--- vintage_vinyl/settings.py
+++ vintage_vinyl/settings.py
@@ -211,16 +211,3 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', 'whsec_tjjcLHjy0hLtquxLuFyGZu51VzKEkQW6')
 DEFAULT_FROM_EMAIL = 'vintage_vinyl@example.com'
-
-if 'DEVELOPMENT' in os.environ:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'seanrobert2010@gmail.com'
-else:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_TLS = True
-    EMAIL_PORT = 587
-    EMAIL_HOST = 'seanrobert2010@gmail.com'
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
-
