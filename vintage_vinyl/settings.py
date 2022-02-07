1eb00609515d2e81f45a0a4b345b9b21dc90bb29
mackey123 <seanrobert2010@gmail.com>
2022-02-07 02:13:29 +0000

    trying to fix error

--- vintage_vinyl/settings.py
+++ vintage_vinyl/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', ''),
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'Development' in os.environ
 
 ALLOWED_HOSTS = ['vintage-vinyl-app.herokuapp.com', 'localhost']
 
@@ -118,12 +118,12 @@ WSGI_APPLICATION = 'vintage_vinyl.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# if 'DATABASE_URL' in os.environ:
-#     DATABASES = {
-#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-#     }
-# else:
-DATABASES = {
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
